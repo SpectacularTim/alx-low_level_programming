5b1faebce583bca128bb1347aac2fd0451d514a9
Timileyin Olawale <timileyinolawale@gmail.com>
2023-06-15 18:14:06 -0700

    Printing my first C message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This prints Programming is like building a multilingual puzzle
+ * return: This returns 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle"/n);
+       return (0);
+}       
