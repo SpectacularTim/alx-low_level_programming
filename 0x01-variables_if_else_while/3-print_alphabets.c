7b258a3bac29f7cccf4554289f1d40091755ea32
Timileyin Olawale <timileyinolawale@gmail.com>
2023-06-17 05:31:59 -0700

    All alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints alphaBETS
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lowercase_alphabets = 'a';
+
+	while (lowercase_alphabets <= 'z')
+	{
+		putchar(lowercase_alphabets);
+		lowercase_alphabets++;
+	}
+	int uppercase_alphabets = 'A';
+
+	while (uppercase_alphabets <= 'Z')
+	{
+		putchar(uppercase_alphabets);
+		uppercase_alphabets++;
+	}
+	putchar('\n');
+	return (0);
+}
