f8bc03790611cdcbbff6efcae138c7404b3469ff
Timileyin Olawale <timileyinolawale@gmail.com>
2023-06-17 05:05:02 -0700

    Small alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (char alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar("\n");
+	Return(0);
+}
